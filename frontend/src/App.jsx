8138654e1768d0ac97f43b9f0ee60c2d279e6768
Annv11022005 <202828644+Annv11022005@users.noreply.github.com>
2026-07-10 00:10:10 +0700

    switch vite

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button';
+
+const App = () => {
+  return (
+    <div>
+      welcome
+      <Button>Click me</Button>
+    </div>
+  );
+};
+
+export default App;
